afd9a546ef7445b52d7ceb3de113aa742bc36a82
Thang0226 <thang.nd0226@gmail.com>
2024-09-14 10:28:03 +0700

    add content

--- clickEvent.js
+++ clickEvent.js
@@ -1,1 +1,12 @@
-let imgObj = document.getElementById();
+let imgObj = document.getElementById("myImage");
+
+window.onload = init;
+function init() {
+  imgObj = document.getElementById("myImage");
+  imgObj.style.position = "relative";
+  imgObj.style.left = "0px";
+}
+
+function moveRight() {
+  imgObj.style.left = parseInt(imgObj.style.left) + 10 + "px";
+}
